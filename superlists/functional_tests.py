0de064b95ab781f53280386d7ea4176af67c8426
Andrew Musali <andrewmusali@gmail.com>
2021-04-09 06:15:17 +0300

    fixes merge conflict

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
     # run selenium using the chrome browser
     def setUp(self):
         self.browser = webdriver.Chrome(
-            'C:/Users/Ec/Desktop/Python/Software Dev practices/tdd_avec_python/browser/chromedriver')
+            'c:/chrome_driver/chromedriver')
 
     def tearDown(self):
         self.browser.quit()
@@ -24,13 +24,10 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_elements_by_tag_name('h1')
-<<<<<<< HEAD
-        for e in header_text:
-            self.assertIn('To-Do', e.get_attribute('innerHTML'))
-=======
+
         for h in header_text:
             self.assertIn('To-Do', h.get_attribute('innerHTML'))
->>>>>>> a25e5e63be89ba4505cc1fb1a11ddc71ee4b2790
+
 
         # She is invited to enter a to-do list right away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -47,17 +44,10 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-<<<<<<< HEAD
-            any(row.text == '1: Buy peacock feathers' for row in rows 
-                "New to-do item did not appear in table"
-                )
-=======
             any(row.text == '1: Buy peacock feathers' for row in rows if
                         "New to-do item did not appear in table"
             )
->>>>>>> a25e5e63be89ba4505cc1fb1a11ddc71ee4b2790
         )
-
         # There's still a textbox inviting her to add another item. She enters "Use Peacock feathers to make a fly"
         # Edith is very methodical
         self.fail('Finish the test')
@@ -71,8 +61,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-    def getMaxWindow(self):
-        self.browser.maximize_window()
+        def getMaxWindow(self):
+            self.browser.maximize_window()
 
 
 if __name__ == '__main__':
